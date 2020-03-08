c4656c0e22aafb9ce8b4731ed6d039e2da9967f3
Leonid Belikov <mail@arfeo.net>
2020-03-08 15:04:10 +0300

    global typings upd

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,3 +1,13 @@
 interface App {
   pageInstance: any;
 }
+
+interface HashMap {
+  [key: string]: any;
+}
+
+interface EventHandler {
+  target: HTMLElement | string;
+  type: string;
+  listener: EventListener;
+}
