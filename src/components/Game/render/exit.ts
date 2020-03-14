f9ab444863356c312675848fe3093ade9c5e9e15
Leonid Belikov <mail@arfeo.net>
2020-03-14 17:52:19 +0300

    game render: exit upd

--- src/components/Game/render/exit.ts
+++ src/components/Game/render/exit.ts
@@ -1,10 +1,12 @@
 import { renderWall, renderWallBase, WallHole } from './wall';
 import { drawRectangle } from '../../../utils/drawing';
 
-type ExitHole = WallHole & {
+interface HoleSize {
   width?: number;
   height?: number;
-};
+}
+
+type ExitHole = WallHole & HoleSize;
 
 function renderExit(x: number, y: number): void {
   renderWall.call(this, x, y);
@@ -15,57 +17,59 @@ function renderExitActive(x: number, y: number, state: number): void {
 
   renderWallBase.call(this, x, y);
 
+  const stateOneSize: HoleSize = {
+    width: this.cellSize / 4,
+    height: this.cellSize / 4,
+  };
+
+  const stateTwoSize: HoleSize = {
+    width: this.cellSize / 2 - this.cellSize / 10,
+    height: this.cellSize / 2 - this.cellSize / 10,
+  };
+
   const wallHoles: { [key: number]: ExitHole[] } = {
     1: [
       {
         left: this.cellSize * x + this.cellSize / 6,
         top: this.cellSize * y + this.cellSize / 6,
-        width: this.cellSize / 4,
-        height: this.cellSize / 4,
+        ...stateOneSize,
       },
       {
         left: this.cellSize * x + this.cellSize - this.cellSize / 6 - this.cellSize / 4,
         top: this.cellSize * y + this.cellSize / 6,
-        width: this.cellSize / 4,
-        height: this.cellSize / 4,
+        ...stateOneSize,
       },
       {
         left: this.cellSize * x + this.cellSize / 6,
         top: this.cellSize * y + this.cellSize - this.cellSize / 6 - this.cellSize / 4,
-        width: this.cellSize / 4,
-        height: this.cellSize / 4,
+        ...stateOneSize,
       },
       {
         left: this.cellSize * x + this.cellSize - this.cellSize / 6 - this.cellSize / 4,
         top: this.cellSize * y + this.cellSize - this.cellSize / 6 - this.cellSize / 4,
-        width: this.cellSize / 4,
-        height: this.cellSize / 4,
+        ...stateOneSize,
       },
     ],
     2: [
       {
         left: this.cellSize * x + this.cellSize / 16,
         top: this.cellSize * y + this.cellSize / 16,
-        width: this.cellSize / 2 - this.cellSize / 10,
-        height: this.cellSize / 2 - this.cellSize / 10,
+        ...stateTwoSize,
       },
       {
         left: this.cellSize * x + this.cellSize / 2 + this.cellSize / 16 - this.cellSize / 20,
         top: this.cellSize * y + this.cellSize / 16,
-        width: this.cellSize / 2 - this.cellSize / 10,
-        height: this.cellSize / 2 - this.cellSize / 10,
+        ...stateTwoSize,
       },
       {
         left: this.cellSize * x + this.cellSize / 16,
         top: this.cellSize * y + this.cellSize / 2 + this.cellSize / 16 - this.cellSize / 20,
-        width: this.cellSize / 2 - this.cellSize / 10,
-        height: this.cellSize / 2 - this.cellSize / 10,
+        ...stateTwoSize,
       },
       {
         left: this.cellSize * x + this.cellSize / 2 + this.cellSize / 16 - this.cellSize / 20,
         top: this.cellSize * y + this.cellSize / 2 + this.cellSize / 16 - this.cellSize / 20,
-        width: this.cellSize / 2 - this.cellSize / 10,
-        height: this.cellSize / 2 - this.cellSize / 10,
+        ...stateTwoSize,
       },
     ],
   };
