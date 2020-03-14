e1ad45f99a9c46968c7ec5d6228f51d170e1852e
Leonid Belikov <mail@arfeo.net>
2020-03-14 18:10:48 +0300

    game: + Alert common component

---
+++ src/components/common/Alert.ts
@@ -0,0 +1,40 @@
+import { ModalComponent } from '../core/Modal';
+import { PageComponent } from '../core/Page';
+import { MenuComponent } from '../core/Menu';
+
+class Alert extends ModalComponent {
+  protected alertLabel: HTMLElement;
+  protected alertSubmitContainer: HTMLElement;
+  protected alertSubmitClose: HTMLButtonElement;
+
+  public constructor(page: PageComponent | MenuComponent, text: string, size?: 'large' | 'medium' | 'small') {
+    super(page, text, size);
+  }
+
+  public init(): void {
+    this.alertLabel = document.createElement('div');
+    this.alertSubmitContainer = document.createElement('div');
+    this.alertSubmitClose = document.createElement('button');
+
+    this.eventHandlers = [
+      {
+        target: this.alertSubmitClose,
+        type: 'click',
+        listener: () => this.destroy(),
+      },
+    ];
+  }
+
+  public render(): void {
+    this.alertLabel.innerText = this.modalContent;
+    this.alertSubmitContainer.className = 'modal-submit';
+    this.alertSubmitClose.className = '-button';
+    this.alertSubmitClose.innerText = 'Close';
+
+    this.modal.appendChild(this.alertLabel);
+    this.modal.appendChild(this.alertSubmitContainer);
+    this.alertSubmitContainer.appendChild(this.alertSubmitClose);
+  }
+}
+
+export { Alert };
