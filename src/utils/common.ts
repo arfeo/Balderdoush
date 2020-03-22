246ee85c29e0cb21b32dda24ae16dadac61c4379
Leonid Belikov <mail@arfeo.net>
2020-03-22 15:22:41 +0300

    common utils: + isObject & isEmpty

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -8,4 +8,26 @@ function getRandomNum(min = 1, max = 1000): number {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
-export { getRandomNum };
+/**
+ * Returns true if the prototype for the `item` param solely comes from `Object`.
+ *
+ * @param item
+ */
+function isObject(item: any): boolean {
+  return !!item && Object.prototype.toString.call(item) === '[object Object]';
+}
+
+/**
+ * Returns true if the prototype for the `item` param solely comes from `Object`, and it has no keys.
+ *
+ * @param item
+ */
+function isEmpty(item: any): boolean {
+  return isObject(item) && Object.keys(item).length === 0;
+}
+
+export {
+  getRandomNum,
+  isObject,
+  isEmpty,
+};
