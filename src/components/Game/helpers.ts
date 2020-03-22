b187b5c6b90c6d21a65c742e05a5b620ccad854b
Leonid Belikov <mail@arfeo.net>
2020-03-22 20:54:03 +0300

    game helpers: getInitialOffset fix

--- src/components/Game/helpers.ts
+++ src/components/Game/helpers.ts
@@ -11,10 +11,10 @@ function getInitialOffset(): number[] {
 
   const [avatarY, avatarX] = items[0];
   const offsetY: number = this.levelMap[avatarY - 9]
-    ? (this.levelMap[avatarY + 3] ? avatarY - 9 : 21 - VISIBLE_MAP_HEIGHT + 1)
+    ? (this.levelMap[avatarY + 3] ? avatarY - 9 : (21 - VISIBLE_MAP_HEIGHT + 1))
     : 0;
-  const offsetX: number = this.levelMap[offsetY][avatarX - 15]
-    ? (this.levelMap[offsetY][avatarX + 4] ? avatarX - 15 : 39 - VISIBLE_MAP_WIDTH + 1)
+  const offsetX: number = this.levelMap[offsetY][avatarX - 15] !== undefined
+    ? (this.levelMap[offsetY][avatarX + 4] !== undefined ? avatarX - 15 : (39 - VISIBLE_MAP_WIDTH + 1))
     : 0;
 
   return [offsetY, offsetX];
