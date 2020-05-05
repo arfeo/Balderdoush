ef2f79043873a4f9487baea039fa39d629303f65
Leonid Belikov <mail@arfeo.net>
2020-05-05 23:06:37 +0300

    game render upd

--- src/components/Game/render/gameBoard.ts
+++ src/components/Game/render/gameBoard.ts
@@ -1,6 +1,5 @@
 import { TOTAL_MAP_HEIGHT, TOTAL_MAP_WIDTH } from '../../../constants/game';
 
-import { renderPanel } from './panel';
 import { renderMap } from './map';
 
 function renderGameBoard(): HTMLElement {
@@ -42,4 +41,21 @@ function renderGameBoard(): HTMLElement {
   return gameContainer;
 }
 
-export { renderGameBoard };
+function renderPanel(): void {
+  this.panelDiamonds.innerText = this.diamondsToGet.toString().padStart(3, '0');
+  this.panelDiamondValue.innerText = this.diamondValue.toString().padStart(4, '0');
+  this.panelScore.innerText = this.score.toString().padStart(6, '0');
+
+  if (this.isPaused) {
+    this.panelTime.classList.add('paused');
+    this.panelTime.innerText = '*PAUSED*';
+  } else {
+    this.panelTime.classList.remove('paused');
+    this.panelTime.innerText = this.time.toString().padStart(3, '0');
+  }
+}
+
+export {
+  renderGameBoard,
+  renderPanel,
+};
--- src/components/Game/render/map.ts
+++ src/components/Game/render/map.ts
@@ -65,7 +65,28 @@ function renderMapItem(x: number, y: number): void {
   }
 }
 
+function rerenderCellWithNeighbors(x: number, y: number): void {
+  const rerenderCoords: number[][] = [
+    [y, x],
+    [y, x + 1],
+    [y + 1, x + 1],
+    [y + 1, x],
+    [y + 1, x - 1],
+    [y, x - 1],
+    [y - 1, x - 1],
+    [y - 1, x],
+    [y - 1, x + 1],
+  ];
+
+  rerenderCoords.forEach((item: number[]) => {
+    const [itemY, itemX] = item;
+
+    renderMapItem.call(this, itemX, itemY);
+  });
+}
+
 export {
   renderMap,
   renderMapItem,
+  rerenderCellWithNeighbors,
 };
